48aac8ee3919851b5832274ceab33c2d243c2c6c
Samantha <samantha.sammy21@gmail.com>
2021-02-09 19:58:24 -0700

    Starting JavaScript Logic

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 console.log("Project 1")
 
+let cardArray = [
+    {name:"purple", img: "https://bit.ly/2OoGT5G"},
+    {name:"tulip", img: "https://bit.ly/3jAsoHB"},
+    {name:"rose", img: "https://bit.ly/3cXvPH1"}
+]
+
+let mainGridImages = document.querySelectorAll('.grid-item')
+
+let restartButton = document.querySelector('.restart-button')
+
+let cardsId = []
+let cardsSelected = []
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    createGameGrid(grid)
+})
