ab40e7e73c3144de65d67866c4c7ad3e3fb1f81a
Samantha <samantha.sammy21@gmail.com>
2021-02-10 18:44:54 -0700

    Working on JavaScript

--- js/app.js
+++ js/app.js
@@ -1,19 +1,86 @@
 console.log("Project 1")
 
-let cardArray = [
+
+//first create an array of my 'cards' aka all my images
+const imagesArray = [
     {name:"purple", img: "https://bit.ly/2OoGT5G"},
     {name:"tulip", img: "https://bit.ly/3jAsoHB"},
     {name:"rose", img: "https://bit.ly/3cXvPH1"}
 ]
 
-let mainGridImages = document.querySelectorAll('.grid-item')
+//the div holding my card image is the grid-item
+const gridItemImages = document.getElementsByClassName('plant-img-front')
 
-let restartButton = document.querySelector('.restart-button')
+const images = [...imagesArray] //The three dots in JavaScript are spread / rest operator... -- Thank you stackoverflow.com 
 
-let cardsId = []
-let cardsSelected = []
+//now time for my loop to add my eventlisteners to each of my cardImages
+for(let i = 0; i < images.length; i++){
+    images[i].addEventListener('click', displayGridImages)
+    console.log(images[i])
+}
+
+// i need to actually display the cards now:
+
+const displayGridImages = ()=>{
+    this.classList.toggle('open')
+    this.classList.toggle('show')
+    this.classList.toggle('disabled')
+}
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  console.log(shuffle(images))
 
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    createGameGrid(grid)
-})
+const wholeBoard = document.querySelector('grid-container')
+
+function startGame(){
+let shuffledCards = shuffle(images)
+
+for(let i = 0; i < shuffledCards.length; i++){
+    [].forEach.call(shuffledCards, function(item){
+        wholeBoard.appendChild(item)
+    })
+}
+}
+
+window.onload = startGame()
+
+
+
+
+
+// let displayImages = ()=>{
+//     this.classList.toggle('show')
+//     this.classList.toggle('open')
+// }
+
+// function shuffle(array){
+//     const currentImages = array.length, randomIndex
+
+//     while(currentImages !== 0){
+//         randomIndex = Math.floor(Math.random()* currentIndex)
+//         currentIndex -= 1
+//         array[currentIndex] = array[randomIndex]
+//     }
+//     return array
+// }
